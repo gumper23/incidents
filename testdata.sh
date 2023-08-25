65ed6e2297e09bb9124ad10a98c7c2f1790179ed
Russ Smith <gumper@gumper.com>
2023-08-25 03:20:47 -0700

    Used RANDOM instead of shuf; 50% time saving

--- testdata.sh
+++ testdata.sh
@@ -19,18 +19,16 @@ TOTAL_DAYS=50
 RANDOM=$(gdate +"%T.%N")
 MAX_INCIDENTS_PER_DAY=3
 
-DATE=$(gdate '+%Y-%m-%d')
+TODAY=$(gdate '+%Y-%m-%d')
 for ((i=0; i<=TOTAL_DAYS; i++))
 do
-    NEXT_DATE=$(gdate +%Y-%m-%d -d "$DATE - $i day")
+    DATE=$(gdate +%Y-%m-%d -d "$TODAY - $i day")
     NUM_INCIDENTS=$(shuf -i 0-${MAX_INCIDENTS_PER_DAY} -n1)
-    echo "-- [${NEXT_DATE}] incidents [${NUM_INCIDENTS}]"
+    echo "-- [${DATE}] incidents [${NUM_INCIDENTS}]"
     for ((j=1; j<=NUM_INCIDENTS; j++))
     do
-        RANDOM_HOUR=$(printf "%02d" "$(shuf -i 0-23 -n1)")
-        RANDOM_MINUTE=$(printf "%02d" "$(shuf -i 0-59 -n1)")
-        RANDOM_SECOND=$(printf "%02d" "$(shuf -i 0-59 -n1)")
+        DATETIME=$(printf "${DATE} %02d:%02d:%02d" $((RANDOM % 24)) $((RANDOM % 60)) $((RANDOM % 60)))
         SEVERITY=$(shuf -i 1-3 -n1)
-        echo "insert into incidents(incident_datetime, severity) values('${NEXT_DATE} ${RANDOM_HOUR}:${RANDOM_MINUTE}:${RANDOM_SECOND}', ${SEVERITY});"
+        echo "insert into incidents(incident_datetime, severity) values('${DATETIME}', ${SEVERITY});"
     done
 done
